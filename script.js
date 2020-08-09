f196266ec79a10fc0a8afc0b4c368309cc5aba87
edksam <paakofisam.sam@gmail.com>
2020-08-09 03:36:49 +0100

    Finished

---
+++ script.js
@@ -0,0 +1,62 @@
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
+
+//Loading Spinner Shown
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+// Remove Loading Spinner
+function complete() {
+  if (!loader.hidden) {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+  }
+}
+
+// Get Quote from API
+async function getQuote() {
+  loading();
+  const proxyUrl = "https://cors-anywhere.herokuapp.com/";
+  const apiUrl =
+    "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json";
+  try {
+    const response = await fetch(proxyUrl + apiUrl);
+    const data = await response.json();
+    //If author is blank
+    if (data.quoteAuthor === "") {
+      authorText.innerText = "Unknown Author";
+    } else {
+      authorText.innerText = data.quoteAuthor;
+    }
+    //Reduce font size forlong quotes
+    if (data.quoteText.length > 50) {
+      quoteText.classList.add("long-quote");
+    } else {
+      quoteText.classList.remove("long-quote");
+    }
+    quoteText.innerText = data.quoteText;
+    // Stop loader, Show Quote
+    complete();
+  } catch (error) {
+    getQuote();
+  }
+}
+
+function tweetQuote() {
+  const quote = quoteText.innerText;
+  const author = authorText.innerText;
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+  window.open(twitterUrl, "_blank");
+}
+//Event Listeners
+newQuoteBtn.addEventListener("click", getQuote);
+twitterBtn.addEventListener("click", tweetQuote);
+//On Load
+
+getQuote();
